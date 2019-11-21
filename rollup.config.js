9a787f312246c1ad039a670240dca98624623fd2
vitalets <vitalets@yandex-team.ru>
2019-11-21 17:19:01 +0300

    add rollup-plugin-analyzer

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,31 @@
 import banner from 'rollup-plugin-banner';
+import analyze from 'rollup-plugin-analyzer';
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' }
-  ],
-  plugins: [
-    banner('<%= pkg.name %> v<%= pkg.version %> by <%= pkg.author.name %>')
-  ]
-};
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    plugins: [
+      getBanner(),
+      analyze({summaryOnly: true}), // analyze once
+    ]
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: pkg.module,
+      format: 'es'
+    },
+    plugins: [
+      getBanner(),
+    ]
+  }
+];
+
+function getBanner() {
+  return banner('<%= pkg.name %> v<%= pkg.version %> by <%= pkg.author.name %>');
+}
